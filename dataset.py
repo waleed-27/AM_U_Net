fcc76971be90876c9f50be9f2d7422384a7700fd
Waleed Bin Tahir <waleedbintahir@yahoo.com>
2024-10-11 21:02:41 +0500

    Updated dataset

--- dataset.py
+++ dataset.py
@@ -1,32 +1,3 @@
-# import torch
-# from torch.utils.data import Dataset
-# from PIL import Image
-# import os
-
-# class RetinalDataset(Dataset):
-#     def __init__(self, image_dir, mask_dir, transform=None):
-#         self.image_dir = image_dir
-#         self.mask_dir = mask_dir
-#         self.transform = transform
-#         self.images = sorted(os.listdir(image_dir))
-#         self.masks = sorted(os.listdir(mask_dir))
-
-#     def __len__(self):
-#         return len(self.images)
-
-#     def __getitem__(self, idx):
-#         img_path = os.path.join(self.image_dir, self.images[idx])
-#         mask_path = os.path.join(self.mask_dir, self.masks[idx])
-#         image = Image.open(img_path).convert("RGB")
-#         mask = Image.open(mask_path).convert("L")
-
-#         if self.transform:
-#             image = self.transform(image)
-#             mask = self.transform(mask)
-
-#         return image, mask
-
-
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
